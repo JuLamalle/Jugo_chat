334922f072f90e0ae87fe01b85a917b76009dbdb
Hugo Bonnaric <hugo.bonnaric@epitech.eu>
2023-01-16 11:56:22 +0100

    Initial Push

---
+++ Server/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import http from 'http';
+import {Server} from 'socket.io';
+import path from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const PORT = 4000;
+const httpServer = http.createServer(app);
+const io = new Server(httpServer,{
+    cors:{
+        origin:["http://localhost:3000"],
+    },
+});
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log("Connection is ready");
+})
+
+httpServer.listen( PORT, () => {
+    console.log('Server is running on http://localhost:4000')
+}); 
