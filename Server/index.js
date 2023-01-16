53b98a2a08e95882e91b6d276aa8257dcc0cb992
Hugo Bonnaric <hugo.bonnaric@epitech.eu>
2023-01-16 16:20:27 +0100

    Chat fonctionnel sur un serveur pour plusieurs clients

--- Server/index.js
+++ Server/index.js
@@ -14,14 +14,22 @@ const io = new Server(httpServer,{
 });
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
+ 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-    console.log("Connection is ready");
-})
+socket.on('send-message',(data)=>{
+    socket.broadcast.emit('message-from-server',data);
+    });
+    socket.on('disconnect', (socket) => {
+        console.log("User left");
+            });
+});
+
+
+    
 
 httpServer.listen( PORT, () => {
     console.log('Server is running on http://localhost:4000')
--- jugochat/src/App.js
+++ jugochat/src/App.js
@@ -1,20 +1,61 @@
-import Button from '@mui/material/Button';
 import { useEffect, useState } from 'react';
-import {io} from 'socket.io-client';
+import { io } from 'socket.io-client';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import  Container  from '@mui/material/Container';
+import  Typography  from '@mui/material/Typography';
+
 
 
 function App() {
 
   const [socket, setSocket] = useState(null);
+  const [message, setMessage] = useState('');
+  const [chat, setChat] = useState([]);
 
   useEffect(() => {
-    setSocket(io('http://localhost:4000'))
+    setSocket(io('http://localhost:4000'));
   }, []);
+
+
+  useEffect(() => {
+    if (!socket) return;
+    socket.on('message-from-server', (data) => {
+      setChat((prev) => [...prev, data.message]);
+    })
+
+  }, [socket]);
+
+
+  function handleForm(e) {
+    e.preventDefault();
+    socket.emit("send-message", { message })
+    setMessage("");
+
+  }
 
   return (
     <div>
-      Hello socket
-      <Button variant="text">Text</Button>
+      <Container>
+        <Box sx={{marginBottom:5}}>
+        {chat.map((message) => (
+            <Typography>{message}</Typography>
+          ))}
+        
+       </Box>
+        <Box component="form" onSubmit={handleForm}>
+
+          <TextField
+            label="Write your message"
+            variant="standard"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+
+          <Button variant="text" type='submit'>Send</Button>
+        </Box>
+      </Container>
     </div>
   );
 }
