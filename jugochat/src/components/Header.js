a0ee68b0c8f3b284e8c87dd6d21dc611b99d32bf
Julien Lamalle <julien1.lamalle@epitech.eu>
2023-01-23 15:23:17 +0100

    dynamic ongoing, duplicate key problem in chat message

--- jugochat/src/components/Header.js
+++ jugochat/src/components/Header.js
@@ -1,24 +1,35 @@
 import React from 'react';
-import { Button, Card } from '@mui/material';
-import { Link } from "react-router-dom";
+import { Button, Card, Box } from '@mui/material';
+import { Link, useNavigate } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 
 
 export default function Header() {
-    const roomId = uuidv4();
-  return (
-    <Card sx={{ margin:5, backgroundColor: "gray"}} raised>
-        <Link style={{textDecoration: "none"}} to ="/">
-            <Button sx={{ color: "white"}} variant="text">Home</Button>
-        </Link>
-        <Link style={{textDecoration: "none"}} to ="/chats">
-            <Button sx={{ color: "white"}} variant="text">Chats</Button>
-        </Link>
-        <Link style={{textDecoration: "none"}} to ={`/room/${roomId}`}>
-            <Button sx={{ color: "white"}} variant="text">
-                Room 1
-            </Button>
-        </Link>
-    </Card>
-  );
+    const navigate = useNavigate();
+    const createNewRoom = () => {
+        const roomId = uuidv4();
+        navigate(`/room/${roomId}`);
+    }
+    return (
+        <Card sx={{ margin: 5, backgroundColor: "gray" }} raised>
+            <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+                <Box>
+                    <Link style={{ textDecoration: "none" }} to="/">
+                        <Button sx={{ color: "white" }} variant="text">Home</Button>
+                    </Link>
+                    <Link style={{ textDecoration: "none" }} to="/chats">
+                        <Button sx={{ color: "white" }} variant="text">General</Button>
+                    </Link>
+                    {/* <Link style={{ textDecoration: "none" }} to={`/room/${roomId}`}>
+                        <Button sx={{ color: "white" }} variant="text">
+                            Room 1
+                        </Button>
+                    </Link> */}
+                </Box>
+                <Button sx={{ color: "white" }} variant="text" onClick={createNewRoom}>
+                    New Room
+                </Button>
+            </Box>
+        </Card>
+    );
 }
