37b83cf6d61e68ca49aab680270bc717f7276720
Julien Lamalle <julien1.lamalle@epitech.eu>
2023-01-28 15:50:58 +0100

    init message model

--- Server/socket/controllers/MessageController.js
+++ Server/socket/controllers/MessageController.js
@@ -1,11 +1,18 @@
 import BaseController from "./BaseController.js";
+import Message from "../../models/Messages.js";
 
 export default class MessageController extends BaseController {
     disconnect = (socket) => {
         console.log("User left");
     }
 
-    sendMessage = ({ message, roomId }) => {
+    sendMessage = ({ message, roomId, messageId, userId }) => {
+        const room = new Room({
+            messageId: 'RoomT',
+            message: 'RoomT',
+            roomId: roomId,
+            userId: userId,
+        });
         let skt = this.socket.broadcast
         skt = roomId ? skt.to(roomId) : skt;
         skt.emit("message-from-server", { message });
