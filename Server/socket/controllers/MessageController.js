20f550c5e8dca4df814b8d453aa9dd90afbd6547
Julien Lamalle <julien1.lamalle@epitech.eu>
2023-01-28 16:32:21 +0100

    messages stored in db on send-message

--- Server/socket/controllers/MessageController.js
+++ Server/socket/controllers/MessageController.js
@@ -2,17 +2,20 @@ import BaseController from "./BaseController.js";
 import Message from "../../models/Messages.js";
 
 export default class MessageController extends BaseController {
+    messageCount = 1;
     disconnect = (socket) => {
         console.log("User left");
     }
 
-    sendMessage = ({ message, roomId, messageId, userId }) => {
-        const room = new Room({
-            messageId: 'RoomT',
-            message: 'RoomT',
+    sendMessage = async ({ message, roomId, userId }) => {
+        const countMessage = await Message.estimatedDocumentCount();
+        const newMessage = new Message({
+            messageId: countMessage+1,
+            message: message,
             roomId: roomId,
             userId: userId,
         });
+        newMessage.save();
         let skt = this.socket.broadcast
         skt = roomId ? skt.to(roomId) : skt;
         skt.emit("message-from-server", { message });
